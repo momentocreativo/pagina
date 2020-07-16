157751370cff06778ed2af88cedf0da5abfd97dd
alejanfoo <68209316+alejanfoo@users.noreply.github.com>
2020-07-16 13:36:00 -0400

    Add files via upload

---
+++ js/contdown.js
@@ -0,0 +1,34 @@
+const getRemainTime = deadline =>{
+  let now = new Date(),
+      remainTime = (new Date (deadline) - now + 1000)/1000,
+      remainSeconds = ('0'+ Math.floor(remainTime % 60) ).slice(-2),
+      remainMinutes = ('0'+ Math.floor(remainTime /60 % 60) ).slice(-2),
+      remainHours = ('0'+ Math.floor(remainTime /3600 % 24) ).slice(-2),
+      remainDays = Math.floor(remainTime /(3600 * 24));
+      
+    return {
+         remainTime,
+        remainSeconds,
+        remainMinutes,
+        remainHours,
+        remainDays
+    }
+    
+};
+
+const countdown = (deadline, elem, finalMessage)=>{
+  const el = document.getElementById(elem);
+  
+    const timerUpdate = setInterval(()=>{
+        let t= getRemainTime(deadline);
+        el.innerHTML=`${t.remainDays}d:${t.remainHours}h:${t.remainMinutes}m:${t.remainSeconds}s`;
+        
+        if(t.remainTime<=1){
+           clearInterval(timerUpdate);
+            el.innerHTML=finalMessage;
+           }
+    },1000)
+};
+
+countdown ('Thu Jul 17 2020 13:16:27 GMT-0400','crono','Ingresa a la cita');
+countdown ('Thu Jul 19 2020 13:16:27 GMT-0400','crono2','Ingresa a la cita');
